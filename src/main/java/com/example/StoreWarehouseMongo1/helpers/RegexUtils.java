93b175b5e10641045b41654958a3101608624581
tasos91 <tasos.sotiriou91@gmail.com>
2020-05-29 00:52:09 +0300

    regexUtils added which checks some inputs. Also test for this class added

--- src/main/java/com/example/StoreWarehouseMongo1/helpers/RegexUtils.java
+++ src/main/java/com/example/StoreWarehouseMongo1/helpers/RegexUtils.java
@@ -1,4 +1,11 @@
 package com.example.StoreWarehouseMongo1.helpers;
 
+import java.util.regex.Pattern;
+
 public class RegexUtils {
+
+    public static boolean filePathRegex(String filePath) {
+        return Pattern.matches("(http(s?):)([/|.|\\w|\\s|-])*\\.(?:jpg|gif|png)", filePath);
+    }
+
 }
--- src/test/java/com/example/StoreWarehouseMongo1/RegexUtilsTest.java
+++ src/test/java/com/example/StoreWarehouseMongo1/RegexUtilsTest.java
@@ -1,4 +1,18 @@
 package com.example.StoreWarehouseMongo1;
 
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.example.StoreWarehouseMongo1.helpers.RegexUtils.filePathRegex;
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
 public class RegexUtilsTest {
+
+    @Test
+    public void fileRegexTest() {
+        boolean i = filePathRegex("https://myanatolimages.s3.eu-central-1.amazonaws.com/10847718_974571419229711_5476717499072881185_o.jpg");
+        assertEquals(true, i);
+        System.out.println("The filepath is: " + i);
+    }
 }
